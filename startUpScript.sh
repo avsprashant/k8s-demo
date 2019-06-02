0efac2c225c7aad313b4aaa04095a3421f92de0e
avsprashant <40791659+avsprashant@users.noreply.github.com>
2019-06-02 23:52:48 +0530

    Delete startUpScript.sh

--- startUpScript.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-kubectl create -f PV.yml
-kubectl create -f PVC.yml
-kubectl create -f secrets.yml
-kubectl create -f helloWorldAppService.yml
-# getting error with helloWorldAppService
-kubectl create -f database-service.yml
